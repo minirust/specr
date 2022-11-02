7512367a87edfe15f8103f1ff22f8b139b1267dd
memoryleak47 <memoryleak47@protonmail.ch>
2022-11-02 15:52:05 +0100

    restructure baselib: start BigInt wrapper

---
+++ template/src/baselib/bigint.rs
@@ -0,0 +1,48 @@
+use std::ops::*;
+
+use num_bigint::ToBigInt;
+
+pub struct BigInt(num_bigint::BigInt);
+
+impl Neg for BigInt {
+	type Output = Self;
+	fn neg(self) -> Self { Self(-self.0) }
+}
+
+impl Add for BigInt {
+	type Output = Self;
+	fn add(self, other: Self) -> Self {
+        Self(self.0 + other.0)
+    }
+}
+
+impl Sub for BigInt {
+	type Output = Self;
+	fn sub(self, other: Self) -> Self {
+        Self(self.0 - other.0)
+    }
+}
+
+impl Mul for BigInt {
+	type Output = Self;
+	fn mul(self, other: Self) -> Self {
+        Self(self.0 * other.0)
+    }
+}
+
+impl PartialEq<usize> for BigInt {
+	fn eq(&self, other: &usize) -> bool {
+        self.0 == other.to_bigint()
+    }
+}
+
+impl BigInt {
+	pub fn checked_div(self, other: BigInt) -> Option<BigInt> {
+        self.0.checked_div().map(Self)
+	}
+
+	pub fn modulo(self, _: Signedness, other: usize) -> BigInt {
+        todo!()
+	}
+}
+
---
+++ template/src/baselib/mod.rs
@@ -0,0 +1,63 @@
+use std::collections::{HashSet, HashMap};
+use crate::mem::BasicMemory;
+use crate::lang::Endianess;
+use crate::prelude::{Signedness, Result, NdResult, TerminationInfo};
+use std::ops::FromResidual;
+
+mod bigint;
+pub use bigint::*;
+
+mod ret;
+pub use ret::*;
+
+pub type List<T> = Vec<T>;
+pub type Set<T> = HashSet<T>;
+pub type Map<K, V> = HashMap<K, V>;
+pub type Memory = BasicMemory;
+
+pub fn default<T: Default>() -> T {
+	T::default()
+}
+
+pub struct Size;
+pub struct Align;
+
+pub struct ArgAbi;
+
+pub struct MyEndianess;
+impl Endianess for MyEndianess {
+    fn decode<const N: usize>(self, _signed: Signedness, _bytes: [u8; N]) -> BigInt { todo!() }
+    fn encode<const N: usize>(self, _signed: Signedness, _int: BigInt) -> Option<[u8; N]> { todo!() }
+
+}
+pub const ENDIANESS: MyEndianess = MyEndianess;
+
+pub type BbName = String;
+pub type LocalName = String;
+pub type FnName = String;
+
+pub const PTR_SIZE: usize = 8;
+
+pub struct Nondet<T>(T);
+pub fn pick<T>(f: impl Fn(T) -> bool) -> Nondet<T> { todo!() }
+pub fn predict<T>(f: impl Fn(T) -> bool) -> Nondet<T> { todo!() }
+
+pub trait PartialOrd2 {
+	fn le(self, other: Self) -> bool;
+}
+
+#[macro_export]
+macro_rules! list {
+	() => { vec![] };
+	($start:expr $(,$a:expr)*) => { vec![$start $(,$a)* ] };
+	($a:expr ; $b:expr) => { vec![$a ; $b] };
+}
+
+// Yeet
+use std::ops::Yeet;
+
+impl<T> FromResidual<Yeet<TerminationInfo>> for NdResult<T> {
+	fn from_residual(Yeet(x): Yeet<TerminationInfo>) -> Self {
+		Nondet(Err(x))
+	}
+}
