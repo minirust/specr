ff8c8e935ff153fc69255e559008081524b23fe0
memoryleak47 <memoryleak47@protonmail.ch>
2022-11-08 18:46:08 +0100

    improve FromResidual implementation

--- template/src/specr/intrinsics.rs
+++ template/src/specr/intrinsics.rs
@@ -1,6 +1,7 @@
 use crate::specr::*;
 
 use std::ops::{Try, FromResidual, ControlFlow};
+use std::convert::Infallible;
 
 pub struct ArgAbi;
 pub type BbName = String;
@@ -11,7 +12,7 @@ pub struct Nondet<T>(pub(in crate::specr) T);
 
 impl<T, E> Try for Nondet<Result<T, E>> {
     type Output = T;
-    type Residual = Nondet<Result<!, E>>;
+    type Residual = Nondet<Result<Infallible, E>>;
 
     fn from_output(output: Self::Output) -> Self {
         Nondet(Ok(output))
@@ -25,9 +26,18 @@ impl<T, E> Try for Nondet<Result<T, E>> {
     }
 }
 
-impl<T, E> FromResidual<Nondet<Result<!, E>>> for Nondet<Result<T, E>> {
-    fn from_residual(residual: Nondet<Result<!, E>>) -> Self {
+impl<T, E> FromResidual<Nondet<Result<Infallible, E>>> for Nondet<Result<T, E>> {
+    fn from_residual(residual: Nondet<Result<Infallible, E>>) -> Self {
         match residual.0 {
+            Ok(x) => match x {},
+            Err(e) => Nondet(Err(e))
+        }
+    }
+}
+
+impl<T, E> FromResidual<Result<Infallible, E>> for Nondet<Result<T, E>> {
+    fn from_residual(residual: Result<Infallible, E>) -> Self {
+        match residual {
             Ok(x) => match x {},
             Err(e) => Nondet(Err(e))
         }
