1a84b9f5afb2afc93d4bbea3a41009f504bb03ba
memoryleak47 <memoryleak47@protonmail.ch>
2022-11-08 17:39:20 +0100

    implement Try for Nondet<Result<_, _>>

--- template/src/lib.rs
+++ template/src/lib.rs
@@ -3,6 +3,7 @@
 #![feature(try_trait_v2_yeet)]
 #![feature(yeet_expr)]
 #![feature(associated_type_defaults)]
+#![feature(never_type)]
 #![allow(unused)]
 
 #[macro_use]
--- template/src/specr/intrinsics.rs
+++ template/src/specr/intrinsics.rs
@@ -1,8 +1,35 @@
 use crate::specr::*;
 
+use std::ops::{Try, FromResidual, ControlFlow};
+
 pub struct ArgAbi;
 pub type BbName = String;
 pub type LocalName = String;
 pub type FnName = String;
 
 pub struct Nondet<T>(pub(in crate::specr) T);
+
+impl<T, E> Try for Nondet<Result<T, E>> {
+    type Output = T;
+    type Residual = Nondet<Result<!, E>>;
+
+    fn from_output(output: Self::Output) -> Self {
+        Nondet(Ok(output))
+    }
+
+    fn branch(self) -> ControlFlow<Self::Residual, Self::Output> {
+        match self.0 {
+            Ok(x) => ControlFlow::Continue(x),
+            Err(e) => ControlFlow::Break(Nondet(Err(e))),
+        }
+    }
+}
+
+impl<T, E> FromResidual<Nondet<Result<!, E>>> for Nondet<Result<T, E>> {
+    fn from_residual(residual: Nondet<Result<!, E>>) -> Self {
+        match residual.0 {
+            Ok(x) => match x {},
+            Err(e) => Nondet(Err(e))
+        }
+    }
+}
