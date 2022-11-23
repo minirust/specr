51280a531d9ed141cb8e87edf515238ccee70973
memoryleak47 <memoryleak47@protonmail.ch>
2022-11-23 20:31:11 +0100

    <No commit-message>

--- src/bb.rs
+++ src/bb.rs
@@ -2,8 +2,8 @@ use crate::*;
 
 pub fn translate_bb(bb: &mir::BasicBlockData) -> mini::BasicBlock {
     mini::BasicBlock {
-        statements: bb.statements.iter().map(|s| translate_stmt(s)).collect(),
-        terminator: translate_terminator(&bb.terminator),
+        statements: bb.statements.iter().map(translate_stmt).collect(),
+        terminator: translate_terminator(bb.terminator()),
     }
 }
 
@@ -11,7 +11,9 @@ fn translate_stmt(_stmt: &mir::Statement) -> mini::Statement {
     todo!()
 }
 
-// does `None` mean fallthrough? in other words, go from bb `i` to bb `i+1`?
-fn translate_terminator(_stmt: &Option<mir::Terminator>) -> mini::Terminator {
-    todo!()
+fn translate_terminator(terminator: &mir::Terminator) -> mini::Terminator {
+    match terminator.kind {
+        mir::TerminatorKind::Return => mini::Terminator::Return,
+        _ => todo!(),
+    }
 }
