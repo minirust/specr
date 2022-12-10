023192682ef5bb1bf18a722290fa4e99c8d8da4f
memoryleak47 <memoryleak47@protonmail.ch>
2022-12-10 19:17:12 +0100

    trait passthrough Mutex fix

--- libspecr/src/gccow/trait_passthrough.rs
+++ libspecr/src/gccow/trait_passthrough.rs
@@ -3,25 +3,25 @@ use crate::*;
 use std::fmt::{Formatter, Debug, Error};
 use std::hash::{Hash, Hasher};
 
-impl<T> Debug for GcCow<T> where T: Debug + GcCompat {
+impl<T> Debug for GcCow<T> where T: Debug + GcCompat + Clone {
     fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), Error> {
-        self.call_ref_unchecked(|t| t.fmt(f))
+        self.get().fmt(f)
     }
 }
 
-impl<T> Hash for GcCow<T> where T: Hash + GcCompat {
+impl<T> Hash for GcCow<T> where T: Hash + GcCompat + Clone {
     fn hash<H>(&self, state: &mut H) where H: Hasher {
-        self.call_ref_unchecked(|t| t.hash(state))
+        self.get().hash(state)
     }
 }
 
-impl<T> PartialEq for GcCow<T> where T: GcCompat + PartialEq {
+impl<T> PartialEq for GcCow<T> where T: GcCompat + PartialEq + Clone {
     fn eq(&self, other: &Self) -> bool {
-        self.call_ref1_unchecked(*other, |s, o| s == o)
+        self.get() == other.get()
     }
 }
 
-impl<T> Eq for GcCow<T> where T: GcCompat + Eq {}
+impl<T> Eq for GcCow<T> where T: GcCompat + Eq + Clone {}
 
 
 impl<T> Default for GcCow<T> where T: Default + GcCompat {
