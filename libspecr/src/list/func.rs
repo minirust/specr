41e4dcaf721a1f5f71e9bcfe61379b02bc2f21d9
memoryleak47 <memoryleak47@protonmail.ch>
2022-12-13 19:28:49 +0100

    add tiny List test

--- libspecr/src/list/func.rs
+++ libspecr/src/list/func.rs
@@ -136,3 +136,13 @@ impl<T: Obj> List<T> {
     }
 
 }
+
+#[test]
+fn test_list() {
+    let mut l = list![1, 2, 3];
+    assert_eq!(l.index_at(0), 1);
+    assert_eq!(l.pop(), Some(3));
+    assert_eq!(l.len(), Int::from(2));
+    l.push(3);
+    assert_eq!(l.len(), Int::from(3));
+}
