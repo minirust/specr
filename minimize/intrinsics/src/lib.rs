9fd10097bf940d39fd3da2094cd8f5265c3f4947
memoryleak47 <memoryleak47@protonmail.ch>
2022-12-13 13:28:30 +0100

    impl tests/rust.sh

--- minimize/intrinsics/src/lib.rs
+++ minimize/intrinsics/src/lib.rs
@@ -1,6 +1,7 @@
 use std::fmt::Display;
 
 /// When used by `minimize`, a call to this function will be replaced by a `CallIntrinsic::PrintStdout`.
+/// The body of this function is mostly used through `tests/rust.sh`.
 pub fn print(t: impl Display) {
     println!("{}", t);
 }
---
+++ minimize/tests/rust.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# This file is used run a test using `rustc` instead of `minimize`.
+
+set -e
+
+TEST_PATH="/tmp/minimize-testing"
+
+[ -z $1 ] && echo "missing file to execute!" && exit 1
+
+arg="$(readlink -f "$1")"
+wd="$(pwd)"
+
+[ ! -d "$TEST_PATH" ] && mkdir "$TEST_PATH"
+[ -f "$TEST_PATH/out" ] && rm "$TEST_PATH/out"
+cp "$wd/../../rust-toolchain.toml" "$TEST_PATH"
+cd "$TEST_PATH"
+
+rustc "$arg" -o out -L "$wd/../intrinsics/target/debug" -l intrinsics -Zalways-encode-mir -Zmir-emit-retag -Zmir-opt-level=0 --cfg=miri -Zextra-const-ub-checks -Cdebug-assertions=off
+./out
