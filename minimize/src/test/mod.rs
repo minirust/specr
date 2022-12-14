adaaf3374dc87322a8ad00930e85a3cc1890be97
memoryleak47 <memoryleak47@protonmail.ch>
2022-12-14 14:33:56 +0100

    add uninit_read test

--- minimize/src/test/mod.rs
+++ minimize/src/test/mod.rs
@@ -62,3 +62,28 @@ fn dead_before_live() {
     let p = program_from_statements(stmts, locals);
     assert_unwell(p);
 }
+
+#[test]
+fn uninit_read() {
+    let l0 = LocalName(Name(0));
+    let l1 = LocalName(Name(1));
+    let stmts = vec![
+        Statement::StorageLive(l0),
+        Statement::StorageLive(l1),
+        Statement::Assign {
+            destination: PlaceExpr::Local(l0),
+            source: ValueExpr::Load {
+                destructive: false,
+                source: specr::GcCow::new(PlaceExpr::Local(l1)),
+            },
+        },
+    ];
+    let pt = PlaceType {
+        ty: Type::Bool,
+        align: Align::from_bytes(1),
+    };
+    let locals = vec![pt, pt];
+
+    let p = program_from_statements(stmts, locals);
+    assert_ub(p, "load at type PlaceType { ty: Bool, align: Align { raw: Small(1) } } but the data in memory violates the validity invariant");
+}
