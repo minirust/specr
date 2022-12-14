df4c53c18fb01fb31dd848a4c4119619500d2bc6
memoryleak47 <memoryleak47@protonmail.ch>
2022-12-14 16:20:16 +0100

    fix obsolete .get() in trait_passthrough

--- libspecr/src/gccow/trait_passthrough.rs
+++ libspecr/src/gccow/trait_passthrough.rs
@@ -5,28 +5,26 @@ use std::hash::{Hash, Hasher};
 
 impl<T> Debug for GcCow<T> where T: Debug + GcCompat + Clone {
     fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), Error> {
-        self.get().fmt(f)
+        self.call_ref_unchecked(|x| x.fmt(f))
     }
 }
 
 impl<T> Hash for GcCow<T> where T: Hash + GcCompat + Clone {
     fn hash<H>(&self, state: &mut H) where H: Hasher {
-        self.get().hash(state)
+        self.call_ref_unchecked(|x| x.hash(state))
     }
 }
 
 impl<T> PartialEq for GcCow<T> where T: GcCompat + PartialEq + Clone {
     fn eq(&self, other: &Self) -> bool {
-        self.get() == other.get()
+        self.call_ref1_unchecked(*other, |x, y| x == y)
     }
 }
 
 impl<T> Eq for GcCow<T> where T: GcCompat + Eq + Clone {}
-
 
 impl<T> Default for GcCow<T> where T: Default + GcCompat {
     fn default() -> Self {
         Self::new(T::default())
     }
 }
-
