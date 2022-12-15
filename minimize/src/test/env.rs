8be03dfd3114ee5e279b35d64ebd9c97901faff3
memoryleak47 <memoryleak47@protonmail.ch>
2022-12-15 21:50:29 +0100

    start working on Program building system & test "no_preserve_padding"

--- minimize/src/test/env.rs
+++ minimize/src/test/env.rs
@@ -54,4 +54,100 @@ pub fn program_from_statements(stmts: Vec<Statement>, locals: Vec<PlaceType>) ->
     }
 }
 
+pub trait TypeConv {
+    fn get_type() -> Type;
+    fn get_align() -> Align;
+    fn get_size() -> Size;
 
+    fn get_ptype() -> PlaceType {
+        PlaceType {
+            ty: Self::get_type(),
+            align: Self::get_align(),
+        }
+    }
+
+    fn get_layout() -> Layout {
+        Layout {
+            size: Self::get_size(),
+            align: Self::get_align(),
+            inhabited: true, // currently there are no uninhabited types in minirust; Type::Enum is not yet supported!
+        }
+    }
+}
+
+macro_rules! type_conv_impl {
+    ($ty:ty, $signed:expr, $size:expr, $align:expr) => {
+        impl TypeConv for $ty {
+            fn get_type() -> Type {
+                Type::Int(IntType { signed: $signed, size: Size::from_bytes($size)})
+            }
+            fn get_align() -> Align {
+                Align::from_bytes($align)
+            }
+            fn get_size() -> Size {
+                Size::from_bytes($size)
+            }
+        }
+    }
+}
+
+type_conv_impl!(u8, Unsigned, 1, 1);
+type_conv_impl!(u16, Unsigned, 2, 2);
+type_conv_impl!(u32, Unsigned, 4, 4);
+type_conv_impl!(u64, Unsigned, 8, 8);
+type_conv_impl!(u128, Unsigned, 16, 8);
+
+type_conv_impl!(i8, Signed, 1, 1);
+type_conv_impl!(i16, Signed, 2, 2);
+type_conv_impl!(i32, Signed, 4, 4);
+type_conv_impl!(i64, Signed, 8, 8);
+type_conv_impl!(i128, Signed, 16, 8);
+
+impl<T: TypeConv> TypeConv for *const T {
+    fn get_type() -> Type {
+        Type::Ptr(PtrType::Raw { pointee: T::get_layout() })
+    }
+    fn get_align() -> Align {
+        Align::from_bytes(8)
+    }
+    fn get_size() -> Size {
+        Size::from_bytes(8)
+    }
+}
+
+pub fn f(x: u32) -> FnName { FnName(Name(x)) }
+pub fn bb(x: u32) -> BbName { BbName(Name(x)) }
+pub fn l(x: u32) -> LocalName { LocalName(Name(x)) }
+
+pub fn local(x: u32) -> PlaceExpr {
+    PlaceExpr::Local(l(x))
+}
+
+pub fn const_int<T: TypeConv>(i: i64) -> ValueExpr {
+    let c = Constant::Int(i.into());
+    let ty = T::get_type();
+
+    ValueExpr::Constant(c, ty)
+}
+
+// is non-destructive.
+pub fn load(source: PlaceExpr) -> ValueExpr {
+    ValueExpr::Load {
+        source: GcCow::new(source),
+        destructive: false,
+    }
+}
+
+pub fn field(root: PlaceExpr, i: u32) -> PlaceExpr {
+    PlaceExpr::Field {
+        root: GcCow::new(root),
+        field: i.into(),
+    }
+}
+
+pub fn deref(v: ValueExpr, ptype: PlaceType) -> PlaceExpr {
+    PlaceExpr::Deref {
+        operand: GcCow::new(v),
+        ptype,
+    }
+}
--- minimize/src/test/mod.rs
+++ minimize/src/test/mod.rs
@@ -7,7 +7,7 @@ use env::*;
 fn too_large_alloc() {
     fn program_alloc(bytes: Int) -> Program {
         let count = bytes;
-        let array = Type::Array { elem: GcCow::new(Type::Bool), count }; 
+        let array = Type::Array { elem: GcCow::new(Type::Bool), count };
 
         let l0 = LocalName(Name(0));
         let stmts = vec![
@@ -93,5 +93,83 @@ fn uninit_read() {
 
         let p = program_from_statements(stmts, locals);
         assert_ub(p, "load at type PlaceType { ty: Bool, align: Align { raw: Small(1) } } but the data in memory violates the validity invariant");
+    });
+}
+
+// see https://github.com/rust-lang/miri/issues/845
+#[test]
+fn no_preserve_padding() {
+    // Pair := Type::Tuple { ... }
+    // Union := Type::Union { f0: Pair, f1: u32 }
+    //
+    // let _0: Union;
+    // let _1: *const u8;
+    // let _2: u8;
+    //
+    // _0.f1 = 0;
+    // _1 = &raw _0.f0;
+    // _1 = load(_1).offset(1)
+    // _2 = *_1;
+
+    run_sequential(|| {
+        let pair_ty = Type::Tuple {
+            fields: list![
+                (Size::from_bytes(0), u8::get_type()),
+                (Size::from_bytes(2), u16::get_type())
+            ],
+            size: Size::from_bits(32),
+        };
+
+        let union_ty = Type::Union {
+            fields: list![
+                (Size::ZERO, pair_ty),
+                (Size::ZERO, u32::get_type())
+            ],
+            chunks: list![(Size::ZERO, Size::from_bytes(4))],
+            size: Size::from_bytes(4),
+        };
+        let union_pty = PlaceType {
+            ty: union_ty,
+            align: Align::from_bytes(4),
+        };
+
+        let locals = vec![
+            union_pty,
+            <*const u8>::get_ptype(),
+            <u8>::get_ptype()
+        ];
+
+        let stmts = vec![
+            Statement::StorageLive(l(0)),
+            Statement::StorageLive(l(1)),
+            Statement::StorageLive(l(2)),
+            Statement::Assign {
+                destination: field(local(0), 1),
+                source: const_int::<u32>(0),
+            },
+            Statement::Assign {
+                destination: local(1),
+                source: ValueExpr::AddrOf {
+                    target: GcCow::new(field(local(0), 0)),
+                    ptr_ty: PtrType::Raw { pointee: <u8>::get_layout() },
+                },
+            },
+            Statement::Assign {
+                destination: local(1),
+                source: ValueExpr::BinOp {
+                    operator: BinOp::PtrOffset { inbounds: true }, // TODO inbounds or not?
+                    left: GcCow::new(load(local(1))),
+                    right: GcCow::new(const_int::<u32>(1)),
+                },
+            },
+            Statement::Assign {
+                destination: local(2),
+                source: load(deref(load(local(1)), <u8>::get_ptype())),
+            },
+        ];
+
+        let p = program_from_statements(stmts, locals);
+        dump_program(&p);
+        assert_ub(p, "");
     });
 }
