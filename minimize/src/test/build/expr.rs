531074b3835f72130fa66e550457d0b2755da054
memoryleak47 <memoryleak47@protonmail.ch>
2022-12-16 14:15:53 +0100

    impl operators in test/build

--- minimize/src/test/build/expr.rs
+++ minimize/src/test/build/expr.rs
@@ -53,6 +53,67 @@ pub fn addr_of(target: PlaceExpr, ptr_ty: PtrType) -> ValueExpr {
 
 // TODO do BinOp & UnOp.
 
+pub fn neg<T: TypeConv>(v: ValueExpr) -> ValueExpr {
+    let Type::Int(t) = T::get_type() else {
+        panic!("int operator received non-int type!");
+    };
+    ValueExpr::UnOp {
+        operator: UnOp::Int(UnOpInt::Neg, t),
+        operand: GcCow::new(v),
+    }
+}
+
+pub fn int_cast<T: TypeConv>(v: ValueExpr) -> ValueExpr {
+    let Type::Int(t) = T::get_type() else {
+        panic!("int operator received non-int type!");
+    };
+    ValueExpr::UnOp {
+        operator: UnOp::Int(UnOpInt::Cast, t),
+        operand: GcCow::new(v),
+    }
+}
+
+pub fn ptr_to_int(v: ValueExpr) -> ValueExpr {
+    ValueExpr::UnOp {
+        operator: UnOp::Ptr2Int,
+        operand: GcCow::new(v),
+    }
+}
+
+pub fn int_to_ptr(v: ValueExpr, t: Type) -> ValueExpr {
+    let Type::Ptr(ptr_ty) = t else {
+        panic!("int_to_ptr requires Type::Ptr argument!");
+    };
+    ValueExpr::UnOp {
+        operator: UnOp::Int2Ptr(ptr_ty),
+        operand: GcCow::new(v),
+    }
+}
+
+fn int_binop<T: TypeConv>(op: BinOpInt, l: ValueExpr, r: ValueExpr) -> ValueExpr {
+    let Type::Int(t) = T::get_type() else {
+        panic!("int operator received non-int type!");
+    };
+    ValueExpr::BinOp {
+        operator: BinOp::Int(op, t),
+        left: GcCow::new(l),
+        right: GcCow::new(r),
+    }
+}
+
+pub fn add<T: TypeConv>(l: ValueExpr, r: ValueExpr) -> ValueExpr { int_binop::<T>(BinOpInt::Add, l, r) }
+pub fn sub<T: TypeConv>(l: ValueExpr, r: ValueExpr) -> ValueExpr { int_binop::<T>(BinOpInt::Sub, l, r) }
+pub fn mul<T: TypeConv>(l: ValueExpr, r: ValueExpr) -> ValueExpr { int_binop::<T>(BinOpInt::Mul, l, r) }
+pub fn div<T: TypeConv>(l: ValueExpr, r: ValueExpr) -> ValueExpr { int_binop::<T>(BinOpInt::Div, l, r) }
+
+pub fn ptr_offset(l: ValueExpr, r: ValueExpr, inbounds: bool) -> ValueExpr {
+    ValueExpr::BinOp {
+        operator: BinOp::PtrOffset { inbounds },
+        left: GcCow::new(l),
+        right: GcCow::new(r),
+    }
+}
+
 pub fn local(x: u32) -> PlaceExpr {
     PlaceExpr::Local(LocalName(Name(x)))
 }
