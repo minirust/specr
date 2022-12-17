101204f22ad73556bd129626e6acd82a48337332
memoryleak47 <memoryleak47@protonmail.ch>
2022-12-17 13:16:16 +0100

    simplfy too_large_alloc

--- minimize/src/test/fail/too_large_alloc.rs
+++ minimize/src/test/fail/too_large_alloc.rs
@@ -2,20 +2,14 @@ use crate::test::*;
 
 #[test]
 fn too_large_alloc() {
-    fn program_alloc(bytes: Int) -> Program {
-        let count = bytes;
-        let ty = array_ty(bool_ty(), count);
-
-        let locals = vec![ptype(ty, align(1))];
-        let stmts = vec![live(0), dead(0)];
-        small_program(&locals, &stmts)
-    }
-
     run_sequential(|| {
-        let large = Int::from(2).pow(BasicMemory::PTR_SIZE.bits());
-        assert_unwell(program_alloc(large));
+        let ty = <[u8; usize::MAX/2+1]>::get_type();
+        let pty = ptype(ty, align(1));
 
-        let small = Int::from(2);
-        assert_stop(program_alloc(small));
+        let locals = vec![pty];
+        let stmts = vec![live(0), dead(0)];
+
+        let prog = small_program(&locals, &stmts);
+        assert_unwell(prog);
     });
 }
