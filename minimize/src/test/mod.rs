171de3e805a9178239ce8eecd25099bfec408d74
memoryleak47 <memoryleak47@protonmail.ch>
2022-12-18 21:47:12 +0100

    minimize: separate fail -> ub, unwell

--- minimize/src/test/mod.rs
+++ minimize/src/test/mod.rs
@@ -4,14 +4,15 @@ mod build;
 use build::*;
 
 mod pass;
-mod fail;
-
-pub fn assert_ub(prog: Program, msg: &str) {
-    assert_eq!(run_program(prog), Outcome::Ub(msg.to_string()));
-}
+mod ub;
+mod unwell;
 
 pub fn assert_stop(prog: Program) {
     assert_eq!(run_program(prog), Outcome::Stop);
+}
+
+pub fn assert_ub(prog: Program, msg: &str) {
+    assert_eq!(run_program(prog), Outcome::Ub(msg.to_string()));
 }
 
 pub fn assert_unwell(prog: Program) {
---
+++ minimize/src/test/unwell/mod.rs
@@ -0,0 +1,4 @@
+mod dead_before_live;
+mod too_large_alloc;
+mod double_live;
+
