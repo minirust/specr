85ddc81d271f80e3ad4e80908bb5647cf1661f01
memoryleak47 <memoryleak47@protonmail.ch>
2023-02-08 17:04:50 +0100

    minitest: add return_intrinsic_no_next test

--- minitest/src/ub/return_.rs
+++ minitest/src/ub/return_.rs
@@ -66,3 +66,25 @@ fn return_no_next() {
     dump_program(&p);
     assert_ub(p, "Return from a function where caller did not specify next block");
 }
+
+
+#[test]
+fn return_intrinsic_no_next() {
+    let locals = [<*const i32>::get_ptype()];
+
+    let b0 = block2(&[
+        &live(0),
+        &Terminator::CallIntrinsic {
+            intrinsic: Intrinsic::Allocate,
+            arguments: list![const_int::<usize>(4), const_int::<usize>(4)],
+            ret: Some(local(0)),
+            next_block: None,
+        }
+    ]);
+
+    let f = function(Ret::No, 0, &locals, &[b0]);
+    let p = program(&[f]);
+    dump_program(&p);
+    assert_ub(p, "Return from an intrinsic where caller did not specify next block");
+}
+
