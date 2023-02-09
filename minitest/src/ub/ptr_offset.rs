d10afc098935c889b67d9a2268960e93035b3693
memoryleak47 <memoryleak47@protonmail.ch>
2023-02-09 14:44:37 +0100

    minitest: add ptr_offset_overflow test

--- minitest/src/ub/ptr_offset.rs
+++ minitest/src/ub/ptr_offset.rs
@@ -96,7 +96,39 @@ fn ptr_offset_no_inbounds() {
     assert_stop(p);
 }
 
-// TODO reproduce "overflowing inbounds pointer arithmetic".
+#[test]
+fn ptr_offset_overflow() {
+    let union_ty = union_ty(&[
+            (size(0), <usize>::get_type()),
+            (size(0), <*const i32>::get_type()),
+        ], size(8));
+    let union_pty = ptype(union_ty, align(8));
+
+    let locals = [ union_pty ];
+
+    let b0 = block2(&[
+        &live(0),
+        &assign(
+            field(local(0), 0),
+            const_int::<usize>(usize::MAX) // this is the largest possible pointer.
+        ),
+        &assign(
+            field(local(0), 1),
+            ptr_offset( // here we add 1 to the largest possible pointer -> overflow.
+                load(field(local(0), 1)),
+                const_int::<usize>(1),
+                InBounds::Yes
+            ),
+        ),
+        &exit()
+    ]);
+
+    let f = function(Ret::No, 0, &locals, &[b0]);
+    let p = program(&[f]);
+    dump_program(&p);
+    assert_ub(p, "overflowing inbounds pointer arithmetic");
+}
+
 
 #[test]
 fn ptr_offset_out_of_bounds() {
