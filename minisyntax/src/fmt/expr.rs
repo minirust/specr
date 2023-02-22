961d283af50fe5013aad2b4c20a2d3740fb22d9a
memoryleak47 <memoryleak47@protonmail.ch>
2023-02-22 16:12:32 +0100

    extend minisyntax fmt

--- minisyntax/src/fmt/expr.rs
+++ minisyntax/src/fmt/expr.rs
@@ -26,7 +26,7 @@ fn constant_to_string(c: Constant) -> String {
     match c {
         Constant::Int(int) => int.to_string(),
         Constant::Bool(b) => b.to_string(),
-        c => format!("{:?}", c),
+        Constant::Variant { .. } => panic!("enums are unsupported!"),
     }
 }
 
@@ -44,10 +44,19 @@ pub fn value_expr_to_string(v: ValueExpr) -> String {
 
             format!("{lparen}{l}{rparen}")
         },
-        ValueExpr::Load { destructive: _, source } => {
+        ValueExpr::Union { field, expr, union_ty } => {
+            let union_ty = type_to_string(union_ty);
+            let expr = value_expr_to_string(expr.get());
+            format!("{union_ty} {{ f{field} : {expr} }}")
+        },
+        ValueExpr::Load { destructive, source } => {
             let source = source.get();
             let source = place_expr_to_string(source);
-            format!("load({source})")
+            let load_name = match destructive {
+                true => "move",
+                false => "load",
+            };
+            format!("{load_name}({source})")
         },
         ValueExpr::AddrOf { target, ptr_ty: PtrType::Raw { .. } } => {
             let target = target.get();
@@ -63,8 +72,18 @@ pub fn value_expr_to_string(v: ValueExpr) -> String {
             };
             format!("&{mutbl}{target}")
         },
-        ValueExpr::AddrOf { target: _, ptr_ty: PtrType::Box { .. } } => {
-            panic!("what? AddrOf with Box?")
+        ValueExpr::AddrOf { target: _, ptr_ty: _ } => {
+            panic!("unsupported ptr_ty for AddrOr!")
+        },
+        ValueExpr::UnOp { operator, operand } => {
+            let operand = value_expr_to_string(operand.get());
+            match operator {
+                UnOp::Int(UnOpInt::Neg, _int_ty) => format!("-{}", operand),
+                UnOp::Int(UnOpInt::Cast, _int_ty) => format!("{} as _", operand),
+                UnOp::Ptr2Ptr(_ptr_ty) => format!("{} as _", operand),
+                UnOp::Ptr2Int => format!("{} as _", operand),
+                UnOp::Int2Ptr(_ptr_ty) => format!("{} as _", operand),
+            }
         },
         ValueExpr::BinOp { operator: BinOp::Int(int_op, int_ty), left, right } => {
             let int_op = match int_op {
@@ -98,7 +117,15 @@ pub fn value_expr_to_string(v: ValueExpr) -> String {
 
             format!("{l} {rel} {r}")
         },
-        v => format!("{:?}", v)
+        ValueExpr::BinOp { operator: BinOp::PtrOffset { inbounds }, left, right } => {
+            let offset_name = match inbounds {
+                true => "offset_inbounds",
+                false => "offset_wrapping",
+            };
+            let l = value_expr_to_string(left.get());
+            let r = value_expr_to_string(right.get());
+            format!("{offset_name}({l}, {r})")
+        }
     }
 }
 
@@ -126,7 +153,12 @@ pub fn type_to_string(t: Type) -> String {
 
             format!("({fields})")
         },
-        t => format!("{:?}", t),
+        Type::Array { elem, count } => {
+            let elem = type_to_string(elem.get());
+            format!("[{}; {}]", elem, count)
+        },
+        Type::Union { .. } => format!("{:?}", t),
+        Type::Enum { .. } => panic!("enums are unsupported!"),
     }
 }
 
