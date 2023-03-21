bb9a16faaff1d4f192a3c7c14c1639c91e707d23
memoryleak47 <memoryleak47@protonmail.ch>
2023-03-21 15:43:57 +0100

    improve block! def; remove redundant specr.toml

--- miniutil/src/build/function.rs
+++ miniutil/src/build/function.rs
@@ -75,44 +75,19 @@ pub fn block(statements: &[Statement], terminator: Terminator) -> BasicBlock {
     }
 }
 
-// like `block`, but a sometimes nicer API.
-pub fn block2(l: &[&dyn ToStmtTerm]) -> BasicBlock {
-    let StmtTerm::Term(term) = l.last().unwrap().to_stmt_term() else { panic!() };
-    let mut stmts = Vec::new();
-    for x in &l[0..l.len() - 1] {
-        let StmtTerm::Stmt(stmt) = x.to_stmt_term() else { panic!() };
-        stmts.push(stmt);
-    }
-
-    BasicBlock {
-        statements: stmts.iter().copied().collect(),
-        terminator: term,
-    }
-}
-
 pub macro block {
-    ($($stmt:expr),* $(,)?) => {
-        block2(&[$(&$stmt),*])
-    }
-}
-
-pub enum StmtTerm {
-    Stmt(Statement),
-    Term(Terminator),
-}
-
-pub trait ToStmtTerm {
-    fn to_stmt_term(&self) -> StmtTerm;
-}
-
-impl ToStmtTerm for Statement {
-    fn to_stmt_term(&self) -> StmtTerm {
-        StmtTerm::Stmt(*self)
-    }
-}
-
-impl ToStmtTerm for Terminator {
-    fn to_stmt_term(&self) -> StmtTerm {
-        StmtTerm::Term(*self)
-    }
+    // entry point
+    ($($rest:expr),* $(,)?) => {
+        block!(@{} $($rest),*)
+    },
+    // exit
+    (@{$($stmts:expr),*} $terminator:expr) => {
+        block(&[$($stmts),*], $terminator)
+    },
+    (@{} $stmt:expr, $($rest:expr),*) => {
+        block!(@{$stmt} $($rest),*)
+    },
+    (@{$($stmts:expr),*} $stmt:expr, $($rest:expr),*) => {
+        block!(@{$($stmts),*, $stmt} $($rest),*)
+    },
 }
