7e018de64e53e63c381ced073df0d320af742447
Yannik Wyss <yawyss@student.ethz.ch>
2023-05-22 08:55:36 +0200

    no_obj

--- specr-transpile/src/auto_derive.rs
+++ specr-transpile/src/auto_derive.rs
@@ -2,40 +2,44 @@ use std::mem;
 
 use crate::prelude::*;
 
-static TRAITS: &[&str] = &["Clone", "Copy", "Debug", "PartialEq", "Eq", "Hash", "GcCompat"];
+/// Traits all structs & enums should derive.
+static GENERAL_TRAITS: &[&str] = &["GcCompat", "Clone", "Debug"];
 
-/// Adds `#[derive(_)]` for all missing traits in `TRAITS`.
+/// Traits only "objects" should derive. They get used in maps, sets, etc.
+static OBJ_TRAITS: &[&str] = &["Copy", "PartialEq", "Eq", "Hash"];
+
+/// Adds `#[derive(_)]` for all missing traits in `GENERAL_TRAITS` and `OBJ_TRAITS`.
 pub fn auto_derive(mut ast: syn::File) -> syn::File {
-    for t in TRAITS {
-        for i in ast.items.iter_mut() {
-            match i {
-                Item::Struct(s) => {
-                    add_derive_attr(t, &mut s.attrs);
-                },
-                Item::Enum(e) => {
-                    add_derive_attr(t, &mut e.attrs);
-                },
-                _ => {},
-            }
-        }
-    }
-
     for i in ast.items.iter_mut() {
-        match i {
+        let attrs = match i {
             Item::Struct(s) => {
-                remove_no_auto_derive(&mut s.attrs);
+                &mut s.attrs
             },
             Item::Enum(e) => {
-                remove_no_auto_derive(&mut e.attrs);
+                &mut e.attrs
             },
-            _ => {},
+            _ => { continue; },
+        };
+
+        for t in GENERAL_TRAITS {
+            add_derive_attr(t, attrs);
+        }
+
+        // If attr `#[no_obj]` is present remove it and skip obj traits.
+        if attrs.iter().any(is_no_obj) {
+            remove_no_obj(attrs);
+            continue;
+        }
+        
+        for t in OBJ_TRAITS {
+            add_derive_attr(t, attrs);
         }
     }
 
     ast
 }
 
-// checks whether `attrs` contains some attribute `#[derive(..., t, ...)]`
+/// checks whether `attrs` contains some attribute `#[derive(..., t, ...)]`
 fn contains_derive_attr(t: &str, attrs: &[Attribute]) -> bool {
     attrs.iter().any(|attr| {
         let Meta::List(list) = &attr.meta else { return false };
@@ -46,32 +50,30 @@ fn contains_derive_attr(t: &str, attrs: &[Attribute]) -> bool {
     })
 }
 
-// checks existence of `#[no_auto_derive]`
-fn is_no_auto_derive(attr: &Attribute) -> bool {
+/// checks if this attribute is `#[no_obj]`
+fn is_no_obj(attr: &Attribute) -> bool {
     let Meta::Path(path) = &attr.meta else {
         return false 
     };
 
-    format!("{}", path.to_token_stream()) == "no_auto_derive"
+    format!("{}", path.to_token_stream()) == "no_obj"
 }
 
-// removes `#[no_auto_derive]`
-fn remove_no_auto_derive(attrs: &mut Vec<Attribute>) {
+/// removes `#[no_obj]`
+fn remove_no_obj(attrs: &mut Vec<Attribute>) {
     let owned_attrs = mem::replace(attrs, vec![]);
-    let owned_attrs = owned_attrs.into_iter().filter(|a| !is_no_auto_derive(a)).collect::<Vec<_>>();
+    let owned_attrs = owned_attrs.into_iter().filter(|a| !is_no_obj(a)).collect::<Vec<_>>();
     _ = mem::replace(attrs, owned_attrs);
 }
 
-// adds `#[derive(t)]` to `attrs`, if it's missing.
+/// adds `#[derive(t)]` to `attrs`, if it's missing.
 fn add_derive_attr(t: &str, attrs: &mut Vec<Attribute>) {
-    if attrs.iter().any(is_no_auto_derive) { return }
-
     if !contains_derive_attr(t, attrs) {
         attrs.push(derive_attr(t));
     }
 }
 
-// generates `#[derive(t)]`
+/// generates `#[derive(t)]`
 fn derive_attr(t: &str) -> Attribute {
     let id = format_ident!("{}", t);
 
