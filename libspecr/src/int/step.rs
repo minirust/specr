3720815edd6fb93f9a8c1b579615372361c2f5e2
Ralf Jung <research@ralfj.de>
2025-05-09 10:51:06 +0200

    bump to Rust 2025-01-01

--- libspecr/src/int/step.rs
+++ libspecr/src/int/step.rs
@@ -3,8 +3,11 @@ use crate::int::*;
 use std::iter::Step;
 
 impl Step for Int {
-    fn steps_between(start: &Self, end: &Self) -> Option<usize> {
-        (*end - *start).try_to_usize()
+    fn steps_between(start: &Self, end: &Self) -> (usize, Option<usize>) {
+        match (*end - *start).try_to_usize() {
+            Some(diff) => (diff, Some(diff)),
+            None => (usize::MAX, None),
+        }
     }
 
     fn forward_checked(start: Self, count: usize) -> Option<Self> {
